3cf8c26dea0f662260b80e43dd3cde6f07c4ca97
cschang07 <manutdsfan@gmail.com>
2021-10-27 17:30:57 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ app.get('/records/:id/edit', (req, res) => {
     )
     .catch(error => console.log(error))
 })
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 app.listen(PORT, () => {
