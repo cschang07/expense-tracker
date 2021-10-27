b13aa224511b6b5f2107d631d2a26addb46891d9
cschang07 <manutdsfan@gmail.com>
2021-10-27 18:06:17 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const Record = require('./models/record')
 
@@ -11,6 +12,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -50,7 +53,21 @@ app.get('/records/:id/edit', (req, res) => {
     )
     .catch(error => console.log(error))
 })
-app.post('/records/:id/delete', (req, res) => {
+app.put('/records/:id', (req, res) => {
+  const id = req.params.id
+  const {name, date, category, amount} = req.body 
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+app.delete('/records/:id', (req, res) => {
   const id = req.params.id
   return Record.findById(id)
     .then(record => record.remove())
