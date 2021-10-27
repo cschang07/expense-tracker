8eb1fe61fbf6fa0f3b17aff7329fdf881320b4cc
cschang07 <manutdsfan@gmail.com>
2021-10-27 14:27:52 +0800

    feat: initiate a seed but not completed yet

---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Record = require('../record')
+const Category = require('../category')
+const User = require('../user')
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
